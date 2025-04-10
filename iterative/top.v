073bc5597503a58beff6bac798c44148054e1ba8
Pop <68675142+PranjalKhatri@users.noreply.github.com>
2025-04-10 14:59:24 +0530

    added ssk

--- iterative/top.v
+++ iterative/top.v
@@ -5,7 +5,9 @@ module top (
     input data_valid,
     input [3:0] data_in,
     output [6:0] result,
-    output done
+    output done,
+    output reg [7:0] an,         // Anode signals for 8 displays
+    output reg [6:0] a_to_g
 );
 
     wire load, accumulate;
@@ -30,5 +32,14 @@ module top (
         .result(result),
         .done(done)
     );
+ seven_segment_display u_seven_segment_display (
+    .clk(clk),                // Connect the 100 MHz clock
+    .reset(rst),            // Connect the reset signal
+    .number1(result),        // Connect the first input number (0-31)
+    .number2(0),        // Connect the second input number (0-31)
+    .an(an),                  // Connect the anode signals (8 displays)
+    .a_to_g(a_to_g)           // Connect the cathode signals (7 segments)
+    );
+
 
 endmodule
